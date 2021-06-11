5bfd201cb104eb07573ac3c9933d01027b6d193f
Kenny Yoon <ghun.yoon@gmail.com>
2021-06-11 02:49:34 -0400

    done

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function Header(props) {
+    return (
+    <header>
+        <h2>Shopster</h2>
+        <button onClick={props.onDarkModeClick}>
+          {props.darkMode ? "Dark" : "Light"} Mode
+        </button>
+    </header>
+    )
+}
+
+
+
+export default Header
